b6b24d04df72e208dfeef79847c929ed681cb373
Dexter Nguyen <lich.nguyen@amblique.com>
2017-10-01 12:47:02 +0700

    Added simple-todos meteor applications

---
+++ imports/ui/body.js
@@ -0,0 +1,60 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+    Meteor.subscribe('tasks');
+    this.state = new ReactiveDict();
+});
+
+Template.body.helpers({
+    tasks() {
+        const instance = Template.instance();
+
+        if (instance.state.get('hideCompleted')) {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }, {
+                sort: {
+                    createAt: -1
+                }
+            })
+        }
+
+        return Tasks.find({}, {
+            sort: {
+                createAt: -1
+            }
+        });
+    },
+    inCompleteCount() {
+        return Tasks.find({
+            checked: {
+                $ne: true
+            }
+        }).count();
+    }
+});
+
+Template.body.events({
+    'submit .new-task'(event) {
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+        Meteor.call('tasks.insert', text);
+
+        target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    }
+});
